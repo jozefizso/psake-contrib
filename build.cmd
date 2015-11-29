7ee8e244ae1c5648245b70d91c10e7246654f03b
Jozef Izso <jozef.izso@gmail.com>
2015-11-29 20:35:26 +0100

    Installing Pester directly from build.cmd script to make the command compatible with Nuget 3.2

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-NuGet.exe install nuget\packages.config -OutputDirectory packages -ExcludeVersion
+NuGet.exe install Pester -OutputDirectory packages -ExcludeVersion -Verbosity quiet
 
 set pester=.\packages\Pester\tools\Pester.psm1
 
